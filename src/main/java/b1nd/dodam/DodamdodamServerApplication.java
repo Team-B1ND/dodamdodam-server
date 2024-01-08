c42d1fc02bf257e2d9484a58b7e3f4ecb365b539
최수원 <suwon3274@gmail.com>
2024-01-08 12:33:10 +0900

    start: 프로젝트 시작

---
+++ src/main/java/b1nd/dodam/DodamdodamServerApplication.java
@@ -0,0 +1,13 @@
+package b1nd.dodam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodamdodamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodamdodamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/b1nd/dodam/DodamdodamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package b1nd.dodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodamdodamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
