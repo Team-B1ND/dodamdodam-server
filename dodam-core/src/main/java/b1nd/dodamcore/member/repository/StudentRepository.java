a5f6e3a6e110e28b08e7dbc9dc728711e9731519
최수원 <suwon3274@gmail.com>
2024-01-10 15:57:04 +0900

    feat: Student 조회 by Member 기능 추가

--- dodam-core/src/main/java/b1nd/dodamcore/member/repository/StudentRepository.java
+++ dodam-core/src/main/java/b1nd/dodamcore/member/repository/StudentRepository.java
@@ -3,12 +3,13 @@ package b1nd.dodamcore.member.repository;
 import b1nd.dodamcore.member.domain.entity.Member;
 import b1nd.dodamcore.member.domain.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Optional<Student> findByMember(Member member);
+
 }
