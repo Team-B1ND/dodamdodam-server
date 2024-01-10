667248e65ed8ba69ce211dec58f5e84808dd682f
최수원 <suwon3274@gmail.com>
2024-01-11 02:50:51 +0900

    feat: Teacher 조회 by Member 기능 추가

--- dodam-core/src/main/java/b1nd/dodamcore/member/repository/TeacherRepository.java
+++ dodam-core/src/main/java/b1nd/dodamcore/member/repository/TeacherRepository.java
@@ -3,13 +3,13 @@ package b1nd.dodamcore.member.repository;
 import b1nd.dodamcore.member.domain.entity.Member;
 import b1nd.dodamcore.member.domain.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+    Optional<Teacher> findByMember(Member member);
 
 }
