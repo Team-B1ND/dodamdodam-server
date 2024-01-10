f3019cb4237921afda4b11c50b78ce0019130e79
최수원 <suwon3274@gmail.com>
2024-01-10 15:21:21 +0900

    feat: ZonedDateTimeUtil 추가

---
+++ dodam-core/src/main/java/b1nd/dodamcore/common/util/ZonedDateTimeUtil.java
@@ -0,0 +1,18 @@
+package b1nd.dodamcore.common.util;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public abstract class ZonedDateTimeUtil {
+
+    public static LocalDateTime nowToLocalDateTime() {
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime();
+    }
+
+    public static LocalTime nowToLocalTime() {
+        return ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalTime();
+    }
+
+}
