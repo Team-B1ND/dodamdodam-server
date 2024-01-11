a13d0f47153666ecf4849e8f3d6d25d639110c28
최수원 <suwon3274@gmail.com>
2024-01-11 10:33:55 +0900

    feat: Validation Exception 처리

--- dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
+++ dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
@@ -5,8 +5,13 @@ import b1nd.dodamcore.common.exception.GlobalExceptionCode;
 import b1nd.dodamcore.common.exception.custom.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,6 +20,19 @@ public class CustomExceptionHandler {
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity handleCustomException(CustomException e){
         return ErrorResponseEntity.responseEntity(e.getExceptionCode());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ResponseEntity handleValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach(
+                error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(errors);
     }
 
     @ExceptionHandler(Exception.class)
