29c9a3df893b2c3977ba37edf33a19373ea7a2e1
최수원 <suwon3274@gmail.com>
2024-02-20 19:11:54 +0900

    feat: HttpMessageNotReadableException Handling 추가

--- dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
+++ dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
@@ -5,13 +5,11 @@ import b1nd.dodamcore.common.exception.GlobalExceptionCode;
 import b1nd.dodamcore.common.exception.custom.CustomException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -22,17 +20,39 @@ public class CustomExceptionHandler {
         return ErrorResponseEntity.responseEntity(e.getExceptionCode());
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity handleValidException(MethodArgumentNotValidException e) {
-        Map<String, String> errors = new HashMap<>();
-
+        StringBuilder message = new StringBuilder();
         e.getBindingResult().getAllErrors().forEach(
-                error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+                error -> message
+                        .append(((FieldError) error).getField()).append(" ")
+                        .append(error.getDefaultMessage()).append(", ")
         );
+
+        log.error("Valid Fail Object : {}", e.getObjectName());
+        log.error("Valid Fail Message : \"{}\"", message.substring(0, message.length() - 2));
 
         return ResponseEntity
                 .status(e.getStatusCode())
-                .body(errors);
+                .body(ErrorResponseEntity.builder()
+                        .status(e.getStatusCode().value())
+                        .code(GlobalExceptionCode.PARAMETER_NOT_VALID.name())
+                        .message(message.substring(0, message.length() - 2))
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(e.toString());
+
+        return ResponseEntity
+                .status(400)
+                .body(ErrorResponseEntity.builder()
+                        .status(GlobalExceptionCode.PARAMETER_NOT_FOUND.getStatus().value())
+                        .code(GlobalExceptionCode.PARAMETER_NOT_FOUND.name())
+                        .message(GlobalExceptionCode.PARAMETER_NOT_FOUND.getMessage())
+                        .build());
     }
 
     @ExceptionHandler(Exception.class)
