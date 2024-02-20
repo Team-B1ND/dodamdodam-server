b78ed8d83852c1bb92c761d38abe1b5922a258c7
최수원 <suwon3274@gmail.com>
2024-02-20 19:36:36 +0900

    refactor: WebClientSupport header parameter 추가

--- dodam-infra/src/main/java/b1nd/dodaminfra/webclient/WebClientSupport.java
+++ dodam-infra/src/main/java/b1nd/dodaminfra/webclient/WebClientSupport.java
@@ -3,14 +3,15 @@ package b1nd.dodaminfra.webclient;
 import b1nd.dodaminfra.webclient.exception.WebClientException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Component
@@ -20,31 +21,39 @@ public class WebClientSupport {
 
     private final WebClient webClient;
 
-    public <T> ResponseEntity<T> get(String url, Class<T> responseDtoClass) {
+    public <T> Mono<T> get(String url, Class<T> responseDtoClass, String... headers) {
         return webClient.method(HttpMethod.GET)
                 .uri(url)
+                .headers(convertStringToHttpHeaders(headers))
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, onError())
-                .toEntity(responseDtoClass)
-                .block();
+                .bodyToMono(responseDtoClass);
     }
 
-    public <T, V> ResponseEntity<T> post(String url, V requestDto, Class<T> responseDtoClass) {
+    public <T, V> Mono<T> post(String url, V requestDto, Class<T> responseDtoClass, String... headers) {
         return webClient.method(HttpMethod.POST)
                 .uri(url)
+                .headers(convertStringToHttpHeaders(headers))
                 .bodyValue(requestDto)
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, onError())
-                .toEntity(responseDtoClass)
-                .block();
+                .bodyToMono(responseDtoClass);
     }
 
     private Function<ClientResponse, Mono<? extends Throwable>> onError() {
         return response -> {
-            log.error("ClientException Status : " + response.statusCode());
-            log.error("ClientException Body : " + response.bodyToMono(Object.class).block());
+            log.error("WebClient Error Status : " + response.statusCode());
+            log.error("WebClient Error Body : " + response.bodyToMono(Object.class).block());
 
             return Mono.error(WebClientException::new);
+        };
+    }
+
+    private Consumer<HttpHeaders> convertStringToHttpHeaders(String... headers) {
+        return httpHeaders -> {
+            for(int i = 0; i<headers.length; i+=2) {
+                httpHeaders.add(headers[i], headers[i+1]);
+            }
         };
     }
 
