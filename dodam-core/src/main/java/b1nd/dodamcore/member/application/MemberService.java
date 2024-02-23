830d0efabfe0f268948d1939b59c8c21a21bfb69
최수원 <suwon3274@gmail.com>
2024-02-23 16:33:36 +0900

    feat: 멤버 (비)활성화 기능 구현

--- dodam-core/src/main/java/b1nd/dodamcore/member/application/MemberService.java
+++ dodam-core/src/main/java/b1nd/dodamcore/member/application/MemberService.java
@@ -4,11 +4,15 @@ import b1nd.dodamcore.auth.application.PasswordEncoder;
 import b1nd.dodamcore.member.application.dto.req.StudentJoinReq;
 import b1nd.dodamcore.member.application.dto.req.TeacherJoinReq;
 import b1nd.dodamcore.member.domain.entity.Member;
+import b1nd.dodamcore.member.domain.entity.Student;
+import b1nd.dodamcore.member.domain.enums.AuthStatus;
+import b1nd.dodamcore.member.domain.event.StudentRegisteredEvent;
 import b1nd.dodamcore.member.domain.exception.MemberDuplicateException;
 import b1nd.dodamcore.member.repository.MemberRepository;
 import b1nd.dodamcore.member.repository.StudentRepository;
 import b1nd.dodamcore.member.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +25,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final StudentRepository studentRepository;
     private final TeacherRepository teacherRepository;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public Member getById(String id) {
         return memberRepository.findById(id)
@@ -33,7 +38,9 @@ public class MemberService {
         String encryptedPassword = checkExistMember(studentJoinReq.id(), studentJoinReq.pw());
 
         Member savedMember = memberRepository.save(studentJoinReq.mapToMember(encryptedPassword));
-        studentRepository.save(studentJoinReq.mapToStudent(savedMember));
+        Student student = studentRepository.save(studentJoinReq.mapToStudent(savedMember));
+
+        publishStudentRegisteredEvent(student);
     }
 
     @Transactional(rollbackFor = Exception.class)
@@ -45,11 +52,21 @@ public class MemberService {
         teacherRepository.save(teacherJoinReq.mapToTeacher(savedMember));
     }
 
+    @Transactional(rollbackFor = Exception.class)
+    public void modifyStatus(String id, AuthStatus status) {
+        Member member = getById(id);
+        member.updateStatus(status);
+    }
+
     private String checkExistMember(String id, String pw) {
         if (memberRepository.existsById(id)) {
             throw new MemberDuplicateException();
         }
         return passwordEncoder.encode(pw);
+    }
+
+    private void publishStudentRegisteredEvent(Student student) {
+        applicationEventPublisher.publishEvent(new StudentRegisteredEvent(student));
     }
 
 }
