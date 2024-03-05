09f74f985a1796604376da2b260195fe9b5cb7c4
최수원 <suwon3274@gmail.com>
2024-03-05 16:12:03 +0900

    feat: SchoolPlace Exception 추가

--- dodam-core/src/main/java/b1nd/dodamcore/common/enums/SchoolPlace.java
+++ dodam-core/src/main/java/b1nd/dodamcore/common/enums/SchoolPlace.java
@@ -1,7 +1,11 @@
 package b1nd.dodamcore.common.enums;
 
+import b1nd.dodamcore.common.exception.ExceptionCode;
+import b1nd.dodamcore.common.exception.custom.CustomException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Arrays;
 import java.util.Objects;
@@ -18,7 +22,41 @@ public enum SchoolPlace {
         return Arrays.stream(values())
                 .filter(value -> Objects.equals(value.place, place))
                 .findAny()
-                .orElseThrow(IllegalArgumentException::new);
+                .orElseThrow(PlaceNotFoundException::new);
+    }
+
+}
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+class PlaceNotFoundException extends CustomException {
+
+    public PlaceNotFoundException() {
+        super(PlaceExceptionCode.NOT_FOUND);
+    }
+
+}
+
+@RequiredArgsConstructor
+enum PlaceExceptionCode implements ExceptionCode {
+
+    NOT_FOUND(HttpStatus.NOT_FOUND, "없는 장소");
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getExceptionName() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
     }
 
 }
