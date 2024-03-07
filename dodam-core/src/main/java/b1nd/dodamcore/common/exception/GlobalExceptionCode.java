34b6309e058b2fe0dc8199d9b110ae9d9e079636
최수원 <suwon3274@gmail.com>
2024-03-07 18:04:03 +0900

    feat: HttpMediaTypeNotSupportedException handling 처리

--- dodam-core/src/main/java/b1nd/dodamcore/common/exception/GlobalExceptionCode.java
+++ dodam-core/src/main/java/b1nd/dodamcore/common/exception/GlobalExceptionCode.java
@@ -10,6 +10,7 @@ public enum GlobalExceptionCode implements ExceptionCode {
 
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 데이터"),
     METHOD_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "잘못된 메서드"),
+    MEDIA_TYPE_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "잘못된 미디어 타입"),
     PARAMETER_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 파라미터"),
     PARAMETER_NOT_VALID(HttpStatus.BAD_REQUEST, "잘못된 파라미터"),
     INVALID_PERMISSION(HttpStatus.BAD_REQUEST, "유효하지 않은 권한"),
--- dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
+++ dodam-core/src/main/java/b1nd/dodamcore/common/exception/handler/CustomExceptionHandler.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -77,6 +78,17 @@ public class CustomExceptionHandler {
                         .status(GlobalExceptionCode.METHOD_NOT_SUPPORTED.getStatus().value())
                         .code(GlobalExceptionCode.METHOD_NOT_SUPPORTED.name())
                         .message(GlobalExceptionCode.METHOD_NOT_SUPPORTED.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    protected ResponseEntity handleHttpMediaTypeNotSupportedException() {
+        return ResponseEntity
+                .status(400)
+                .body(ErrorResponseEntity.builder()
+                        .status(GlobalExceptionCode.MEDIA_TYPE_NOT_SUPPORTED.getStatus().value())
+                        .code(GlobalExceptionCode.MEDIA_TYPE_NOT_SUPPORTED.name())
+                        .message(GlobalExceptionCode.MEDIA_TYPE_NOT_SUPPORTED.getMessage())
                         .build());
     }
 
