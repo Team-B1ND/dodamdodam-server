e1c08a6b04bf732c0dec0e7806d1771b803a24b1
이지민 <wlals060330@gmail.com>
2024-03-08 14:58:57 +0900

    chore: wakeupSong dto createAt 추가

--- dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/application/dto/res/WakeupSongRes.java
+++ dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/application/dto/res/WakeupSongRes.java
@@ -3,13 +3,15 @@ package b1nd.dodamcore.wakeupsong.application.dto.res;
 import b1nd.dodamcore.wakeupsong.domain.entity.WakeupSong;
 import b1nd.dodamcore.wakeupsong.domain.enums.WakeupSongStatus;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record WakeupSongRes(int id,
                             String thumbnailUrl,
                             String videoTitle, String videoId, String videoUrl,
                             String channelTitle,
-                            WakeupSongStatus status) {
+                            WakeupSongStatus status,
+                            LocalDate createdAt) {
     public static List<WakeupSongRes> of(List<WakeupSong> wakeupSongs) {
         return wakeupSongs.stream()
                 .map(WakeupSongRes::of)
@@ -22,7 +24,8 @@ public record WakeupSongRes(int id,
                 wakeupSong.getThumbnailUrl(),
                 wakeupSong.getVideoTitle(), wakeupSong.getVideoId(), wakeupSong.getVideoUrl(),
                 wakeupSong.getChannelTitle(),
-                wakeupSong.getStatus()
+                wakeupSong.getStatus(),
+                wakeupSong.getCreatedAt().toLocalDate()
         );
     }
 }
