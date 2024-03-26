08fc73ae60ad79f3774d761cf03f131bcdc13aed
이지민 <wlals060330@gmail.com>
2024-03-26 11:22:56 +0900

    chore: fix naming

--- dodam-core/src/main/java/b1nd/dodamcore/recruit/application/dto/res/RecruitPageRes.java
+++ dodam-core/src/main/java/b1nd/dodamcore/recruit/application/dto/res/RecruitPageRes.java
@@ -6,19 +6,19 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record RecruitPageRes(List<RecruitListRes> recruit, Integer nextPage) {
+public record RecruitPageRes(List<RecruitListRes> recruitList, Integer nextPage) {
 
     public static RecruitPageRes of(Page<Recruit> recruitPage, Integer nextPage) {
-        return new RecruitPageRes(recruitPage.getContent().stream().map(r ->
+        return new RecruitPageRes(recruitPage.getContent().stream().map(recruit ->
                 new RecruitListRes(
-                        r.getId(),
-                        r.getWriter().getName(),
-                        r.getName(),
-                        r.getLocation(),
-                        r.getDuty(),
-                        r.getEtc(),
-                        r.getPersonnel(),
-                        r.getImage()
+                        recruit.getId(),
+                        recruit.getWriter().getName(),
+                        recruit.getName(),
+                        recruit.getLocation(),
+                        recruit.getDuty(),
+                        recruit.getEtc(),
+                        recruit.getPersonnel(),
+                        recruit.getImage()
                 )).collect(Collectors.toList()),
                 nextPage);
     }
