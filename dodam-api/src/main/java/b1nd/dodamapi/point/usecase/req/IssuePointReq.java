99eadde7f101f8a7024d5341db85b35b70516adc
최수원 <suwon3274@gmail.com>
2024-04-03 13:44:31 +0900

    remove: @Empty로 대체

--- dodam-api/src/main/java/b1nd/dodamapi/point/usecase/req/IssuePointReq.java
+++ dodam-api/src/main/java/b1nd/dodamapi/point/usecase/req/IssuePointReq.java
@@ -1,9 +1,9 @@
 package b1nd.dodamapi.point.usecase.req;
 
-import b1nd.dodamapi.common.validator.NotEmptyList;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record IssuePointReq(@NotNull LocalDate issueAt, @NotNull Integer reasonId, @NotEmptyList List<Integer> studentIds) {}
+public record IssuePointReq(@NotNull LocalDate issueAt, @NotNull Integer reasonId, @NotEmpty List<Integer> studentIds) {}
