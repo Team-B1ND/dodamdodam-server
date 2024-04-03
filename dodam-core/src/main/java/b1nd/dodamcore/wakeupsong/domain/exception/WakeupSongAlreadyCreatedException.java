d84221a403a048eed7f5f4f137f07c1c65f5c5ba
이지민 <wlals060330@gmail.com>
2024-04-03 13:55:34 +0900

    chore: http status 수정

--- dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/domain/exception/WakeupSongAlreadyCreatedException.java
+++ dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/domain/exception/WakeupSongAlreadyCreatedException.java
@@ -4,7 +4,7 @@ import b1nd.dodamcore.common.exception.custom.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.IM_USED)
+@ResponseStatus(value = HttpStatus.LOCKED)
 public class WakeupSongAlreadyCreatedException extends CustomException {
 
     public WakeupSongAlreadyCreatedException() {
--- dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/domain/exception/WakeupSongExceptionCode.java
+++ dodam-core/src/main/java/b1nd/dodamcore/wakeupsong/domain/exception/WakeupSongExceptionCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum WakeupSongExceptionCode implements ExceptionCode {
 
-    ALREADY_APPLIED(HttpStatus.IM_USED, "이미 이번주에 기상송을 신청함"),
+    ALREADY_APPLIED(HttpStatus.LOCKED, "이미 이번주에 기상송을 신청함"),
     NOT_FOUND(HttpStatus.NOT_FOUND, "없는 기상송"),
     NOT_APPLICANT(HttpStatus.FORBIDDEN, "신청자가 아님"),
     URL_MALFORMED(HttpStatus.BAD_REQUEST, "잘못된 유튜브 URL 형식");
