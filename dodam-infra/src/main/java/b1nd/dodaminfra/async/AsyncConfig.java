880833ecffde92f88c64ab44b218dcd5f2a67e07
최수원 <suwon3274@gmail.com>
2024-04-05 12:37:01 +0900

    remove: AsyncConfig 필요없는 코드 제거

--- dodam-infra/src/main/java/b1nd/dodaminfra/async/AsyncConfig.java
+++ dodam-infra/src/main/java/b1nd/dodaminfra/async/AsyncConfig.java
@@ -2,7 +2,7 @@ package b1nd.dodaminfra.async;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -11,10 +11,11 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 @EnableAsync
-class AsyncConfig implements AsyncConfigurer {
+class AsyncConfig {
 
-    @Override
-    public Executor getAsyncExecutor() {
+    @Bean
+    @Primary
+    public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(20);
         executor.setMaxPoolSize(50);
@@ -23,11 +24,6 @@ class AsyncConfig implements AsyncConfigurer {
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
-    }
-
-    @Bean
-    public Executor asyncExecutor() {
-        return getAsyncExecutor();
     }
 
 }
