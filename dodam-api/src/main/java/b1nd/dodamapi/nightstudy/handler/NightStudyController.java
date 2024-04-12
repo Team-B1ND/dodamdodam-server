54c5a615863dac4b3e16b0f4110c91cd2b10e36f
최수원 <suwon3274@gmail.com>
2024-04-12 10:44:53 +0900

    refactor: NightStudy Service -> UseCase

--- dodam-api/src/main/java/b1nd/dodamapi/nightstudy/handler/NightStudyController.java
+++ dodam-api/src/main/java/b1nd/dodamapi/nightstudy/handler/NightStudyController.java
@@ -1,11 +1,11 @@
-package b1nd.dodamapi.nightstudy;
+package b1nd.dodamapi.nightstudy.handler;
 
 import b1nd.dodamapi.common.response.Response;
 import b1nd.dodamapi.common.response.ResponseData;
+import b1nd.dodamapi.nightstudy.usecase.NightStudyUseCase;
 import b1nd.dodamcore.nightstudy.application.NightStudyService;
 import b1nd.dodamcore.nightstudy.application.dto.req.ApplyNightStudyReq;
 import b1nd.dodamcore.nightstudy.application.dto.req.RejectNightStudyReq;
-import b1nd.dodamcore.nightstudy.domain.enums.NightStudyStatus;
 import b1nd.dodamcore.nightstudy.application.dto.res.NightStudyRes;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,56 +18,47 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NightStudyController {
 
+    private final NightStudyUseCase useCase;
     private final NightStudyService nightStudyService;
 
     @PostMapping
     public Response apply(@RequestBody @Valid ApplyNightStudyReq req) {
-        nightStudyService.apply(req);
-
-        return Response.ok("심야자습 신청 성공");
+        return useCase.apply(req);
     }
 
     @DeleteMapping("/{id}")
     public Response cancel(@PathVariable Long id) {
-        nightStudyService.cancel(id);
-
-        return Response.ok("심야자습 취소 성공");
+        return useCase.cancel(id);
     }
 
     @PatchMapping("/{id}/allow")
     public Response allow(@PathVariable Long id) {
-        nightStudyService.modifyStatus(id, NightStudyStatus.ALLOWED, null);
-
-        return Response.ok("심야자습 승인 성공");
+        return useCase.allow(id);
     }
 
     @PatchMapping("/{id}/reject")
     public Response reject(@PathVariable Long id, @RequestBody RejectNightStudyReq req) {
-        nightStudyService.modifyStatus(id, NightStudyStatus.REJECTED, req.rejectReason());
-
-        return Response.ok("심야자습 거절 성공");
+        return useCase.reject(id, req);
     }
 
     @PatchMapping("/{id}/revert")
     public Response revert(@PathVariable Long id) {
-        nightStudyService.modifyStatus(id, NightStudyStatus.PENDING, null);
-
-        return Response.ok("심야자습 대기 성공");
+        return useCase.revert(id);
     }
 
     @GetMapping
     public ResponseData<List<NightStudyRes>> getValid() {
-        return ResponseData.ok("유요한 심야자습 조회 성공", nightStudyService.getValid());
+        return useCase.getValid();
     }
 
     @GetMapping("/my")
     public ResponseData<List<NightStudyRes>> getMy() {
-        return ResponseData.ok("내 심야자습 조회 성공", nightStudyService.getMy());
+        return useCase.getMy();
     }
 
     @GetMapping("/pending")
     public ResponseData<List<NightStudyRes>> getPending() {
-        return ResponseData.ok("대기중인 심야자습 조회 성공", nightStudyService.getPending());
+        return useCase.getPending();
     }
 
 }
