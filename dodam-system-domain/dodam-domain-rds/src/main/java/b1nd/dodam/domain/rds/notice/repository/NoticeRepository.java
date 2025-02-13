9abe44ff357eb9b341f4d1db8db5dd5f1d78bd0a
김가영 <yeong.kmj@gmail.com>
2025-02-13 20:48:15 +0900

    chore: jpql

--- dodam-system-domain/dodam-domain-rds/src/main/java/b1nd/dodam/domain/rds/notice/repository/NoticeRepository.java
+++ dodam-system-domain/dodam-domain-rds/src/main/java/b1nd/dodam/domain/rds/notice/repository/NoticeRepository.java
@@ -11,24 +11,28 @@ import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
-    @Query("select n from notice n " +
-            "join n.noticeDivisions nd " +
-            "join nd.division d " +
-            "join division_member dm on dm.division = d " +
-            "where dm.member.id = :memberId " +
-            "and d.id = :divisionId " +
-            "and n.id > :lastId")
+    @Query("""
+        select n from notice n
+        join n.noticeDivisions nd
+        join nd.division d
+        join division_member dm on dm.division = d
+        where dm.member.id = :memberId
+        and d.id = :divisionId
+        and n.id > :lastId
+        """)
     List<Notice> findNoticesByMemberAndDivision(@Param("memberId") String memberId,
                                                 @Param("divisionId") Long divisionId,
                                                 @Param("lastId") Long lastId,
                                                 Pageable pageable);
 
-    @Query("select n from notice n " +
-            "join n.noticeDivisions nd " +
-            "WHERE nd.division.id in :ids " +
-            "and n.noticeStatus = :noticeStatus " +
-            "and n.id > :lastId " +
-            "and n.title like %:keyword%")
+    @Query("""
+        select n from notice n
+        join n.noticeDivisions nd
+        where nd.division.id in :ids
+        and n.noticeStatus = :noticeStatus
+        and n.id > :lastId
+        and n.title like %:keyword%
+        """)
     List<Notice> findAllByNoticeStatus(@Param("keyword") String keyword,
                                        @Param("ids") List<Long> ids,
                                        @Param("noticeStatus") NoticeStatus noticeStatus,
