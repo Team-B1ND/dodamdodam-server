bb07645627c807e62253a85a8e5555382ce10e22
kmg <legolove08@naver.com>
2025-02-18 01:26:18 +0900

    feat: Create club entity

---
+++ dodam-system-domain/dodam-domain-rds/src/main/java/b1nd/dodam/domain/rds/club/entity/Club.java
@@ -0,0 +1,34 @@
+package b1nd.dodam.domain.rds.club.entity;
+
+import b1nd.dodam.domain.rds.club.enumeration.ClubType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "club")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @NotNull
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @NotNull
+    private String subject;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private ClubType type;
+}
---
+++ dodam-system-domain/dodam-domain-rds/src/main/java/b1nd/dodam/domain/rds/club/repository/ClubRepository.java
@@ -0,0 +1,7 @@
+package b1nd.dodam.domain.rds.club.repository;
+
+import b1nd.dodam.domain.rds.club.entity.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<Club, Long> {
+}
