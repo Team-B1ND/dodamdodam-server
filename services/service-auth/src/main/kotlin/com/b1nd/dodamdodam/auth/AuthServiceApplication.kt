7cd2e6210927fd7e6752b8e8e4ef0e3f85489d31
kmg <legolove08@naver.com>
2026-02-21 20:15:55 +0900

    chore: application 수정

--- services/service-auth/src/main/kotlin/com/b1nd/dodamdodam/auth/AuthServiceApplication.kt
+++ services/service-auth/src/main/kotlin/com/b1nd/dodamdodam/auth/AuthServiceApplication.kt
@@ -1,13 +1,17 @@
 package com.b1nd.dodamdodam.auth
 
+import com.b1nd.dodamdodam.core.kafka.annotation.EnableDodamKafka
 import com.b1nd.dodamdodam.core.security.annotation.EnableDodamSecurity
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication(scanBasePackages = ["com.b1nd.dodamdodam"])
 @EnableDodamSecurity
+@EnableDodamKafka
+@EnableJpaAuditing
 @EnableWebSecurity
 @ConfigurationPropertiesScan
 class AuthServiceApplication
