4b83863a10071ad130ecea8fd7f61eca47017f7d
kmg <legolove08@naver.com>
2026-02-26 14:57:43 +0100

    fix: swagger jwt 추가

--- core/core-common/src/main/kotlin/com/b1nd/dodamdodam/core/common/swagger/configuration/CoreSwaggerConfiguration.kt
+++ core/core-common/src/main/kotlin/com/b1nd/dodamdodam/core/common/swagger/configuration/CoreSwaggerConfiguration.kt
@@ -2,6 +2,9 @@ package com.b1nd.dodamdodam.core.common.swagger.configuration
 
 import com.b1nd.dodamdodam.core.common.swagger.properties.SwaggerProperties
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -15,6 +18,16 @@ class CoreSwaggerConfiguration {
     @Bean
     @ConditionalOnProperty(prefix = "app.swagger", name = ["enabled"], havingValue = "true")
     @ConditionalOnMissingBean(OpenAPI::class)
-    fun openApi(swaggerProperties: SwaggerProperties): OpenAPI =
-        OpenAPI().servers(listOf(Server().url(swaggerProperties.gatewayPrefix)))
+    fun openApi(swaggerProperties: SwaggerProperties): OpenAPI = OpenAPI()
+        .servers(listOf(Server().url(swaggerProperties.gatewayPrefix)))
+        .components(
+            Components().addSecuritySchemes(
+                "bearerAuth",
+                SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            )
+        )
+        .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
 }
--- services/service-user/src/main/kotlin/com/b1nd/dodamdodam/user/UserServiceApplication.kt
+++ services/service-user/src/main/kotlin/com/b1nd/dodamdodam/user/UserServiceApplication.kt
@@ -1,5 +1,6 @@
 package com.b1nd.dodamdodam.user
 
+import com.b1nd.dodamdodam.core.common.swagger.annotation.EnableDodamSwagger
 import com.b1nd.dodamdodam.core.kafka.annotation.EnableDodamKafka
 import com.b1nd.dodamdodam.core.security.annotation.EnableDodamSecurity
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 @EnableJpaAuditing
 @EnableDodamSecurity
 @ConfigurationPropertiesScan
+@EnableDodamSwagger
 class UserServiceApplication
 
 fun main(args: Array<String>) {
