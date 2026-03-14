2e57f28e48a12a95f2a70c4ce3005ae33e8590f9
kmg <legolove08@naver.com>
2026-03-14 20:14:42 +0900

    fix: cors disable

--- services/service-gateway/src/main/kotlin/com/b1nd/dodamdodam/gateway/infrastructure/gateway/config/SecurityConfig.kt
+++ services/service-gateway/src/main/kotlin/com/b1nd/dodamdodam/gateway/infrastructure/gateway/config/SecurityConfig.kt
@@ -5,18 +5,35 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig {
-    
+
     @Bean
     fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
             .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .authorizeExchange { exchanges ->
                 exchanges.anyExchange().permitAll()
             }
             .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration().apply {
+            allowedOriginPatterns = listOf("*")
+            allowedMethods = listOf("*")
+            allowedHeaders = listOf("*")
+            allowCredentials = true
+        }
+        return UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", configuration)
+        }
     }
 }
