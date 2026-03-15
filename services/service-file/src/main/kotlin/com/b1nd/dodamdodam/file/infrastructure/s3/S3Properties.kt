869015ca7f98c7c91268e739f22e986f2a919be2
kmg <legolove08@naver.com>
2026-03-15 21:28:09 +0900

    ci: file dev 서버 배포 파이프라인 추가

--- services/service-file/src/main/kotlin/com/b1nd/dodamdodam/file/infrastructure/s3/S3Properties.kt
+++ services/service-file/src/main/kotlin/com/b1nd/dodamdodam/file/infrastructure/s3/S3Properties.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties("cloud.aws.s3")
 data class S3Properties(
     val endpoint: String = "",
-    val region: String = "ap-northeast-2",
+    val region: String,
     val bucket: String,
     val accessKey: String = "",
     val secretKey: String = "",
