f12d131629147dbccd3c435ce40f0d73e0a90be3
kmg <legolove08@naver.com>
2026-03-15 22:57:18 +0900

    hotfix: 직렬화 문제 수정

--- services/service-user/src/main/kotlin/com/b1nd/dodamdodam/user/infrastructure/cache/CacheConfig.kt
+++ services/service-user/src/main/kotlin/com/b1nd/dodamdodam/user/infrastructure/cache/CacheConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,6 +24,7 @@ class CacheConfig(
     fun cacheManager(): RedisCacheManager {
         val objectMapper = ObjectMapper().apply {
             registerModule(JavaTimeModule())
+            registerModule(KotlinModule.Builder().build())
             activateDefaultTyping(
                 BasicPolymorphicTypeValidator.builder()
                     .allowIfBaseType(Any::class.java)
